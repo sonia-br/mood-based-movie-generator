9257e9769e3a18f1402fdea061874fa8b6b5033f
sonia-br <snbrmaej@gmail.com>
2025-06-13 20:33:18 +0200

    feat: add basic sliders to home page to set mood

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom';
+import MoodSlider from '../components/MoodSlider';
+
 
 function Home()
 {
@@ -6,11 +8,36 @@ function Home()
         <div>
             <h1>What's your mood today?</h1>
             <h3>Select at least 4 sliders</h3>
-            <p>Mood sliders will go here</p>
-            <div>
-                <Link to="/mood-results">
-                    <button>Search by mood</button>
-                </Link> 
+            <section className="mood-sliders">
+                <MoodSlider
+                labelLeft="Sad"
+                labelRight="Happy"
+                moodName="happiness"
+                onChange={(name, value) => console.log(name,value)}
+                />
+            <MoodSlider
+                labelLeft="Calm"
+                labelRight="Excited"
+                moodName="excitement"
+                onChange={(name, value) => console.log(name,value)}
+                />
+            <MoodSlider
+                labelLeft="Dark"
+                labelRight="Light"
+                moodName="lightness"
+                onChange={(name, value) => console.log(name,value)}
+                />
+            <MoodSlider
+                labelLeft="Simple"
+                labelRight="Complex"
+                moodName="simplicity"
+                onChange={(name, value) => console.log(name,value)}
+                /></section>
+            
+            <div className="content__buttons">
+                    <Link to="/mood-results">
+                        <button>Search by mood</button>
+                    </Link> 
                 <Link to="/random">
                     <button>Random movie</button>
                 </Link> 
