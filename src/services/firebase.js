02a2b4ad3018f4df35489db28398e18620bc6293
sonia-br <snbrmaej@gmail.com>
2025-07-07 01:06:01 +0200

    fix: update configurations to deploy correctly

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const db = getFirestore(app);
+const firebaseConfig =
+{
+    apiKey: "AIzaSyAtsYcMD_Of_IxGYLiMOqwdB_tiSO6TDYM",
+  authDomain: "mood-based-movie-generator.firebaseapp.com",
+  projectId: "mood-based-movie-generator",
+  storageBucket: "mood-based-movie-generator.firebasestorage.app",
+  messagingSenderId: "10140873947",
+  appId: "1:10140873947:web:c49fab12695e81418988fe"
+}
 
-export { db };
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, db, provider };
